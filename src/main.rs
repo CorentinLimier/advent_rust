08ee951704e9cb6869ab5c53baf5a2742abfb73b
Corentin Limier <corentin@castordoc.com>
2022-12-02 00:06:13 +0100

    panic!

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,16 @@ mod days;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    
+    if args.len() == 1{
+        panic!("argument day is required");
+    }
 
     let day = &args[1];
 
     if day == "01" {
         days::day01::main();
     } else {
-        println!("day not supported")
+        panic!("day not supported");
     }
 }
